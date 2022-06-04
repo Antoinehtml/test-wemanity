17c3d5b6233318f277a2438a83942a3217d0d7aa
Antoine.HTML <76158618+Antoinehtml@users.noreply.github.com>
2022-06-04 11:35:04 +0200

    slicing all-entries page

--- src/pages/all-entries.js
+++ src/pages/all-entries.js
@@ -1,13 +1,12 @@
-import {
-    Heading,
-} from "@chakra-ui/react";
+import { Flex, Heading, LinkBox, LinkOverlay, Text } from "@chakra-ui/react";
+
 
 import Col from "../comps/Misc/Col";
 import Container from "../comps/Misc/Container";
 
 const AllEntries = () => {
     return (
-        <Container>
+        <Container bg="darkBlue" color="primary" h="calc(100vh - var(--top-bar-height))">
             <Col
                 colStart={3}
                 colEnd={25}
@@ -17,6 +16,10 @@ const AllEntries = () => {
                 alignItems="center"
             >
                 <Heading mb={{ base: 4, lg: 8 }}>All Entries</Heading>
+
+                <Flex mt={10}>
+
+                </Flex>
 
             </Col>
         </Container>
